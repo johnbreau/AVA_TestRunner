8224647fe683cbc7bd3966e57e9d86f14d5756c7
John Breau <john.breau@vml.com>
2017-04-11 22:25:26 -0500

    updated test.js

--- test.js
+++ test.js
@@ -1,10 +1,22 @@
+import Rx from 'rxjs/Rx';
 import test from 'ava';
 
-test('foo', t=> {
+test('Test One:', t => {
   t.pass();
 });
 
-test('bar', async t => {
+test.todo('check to see if observable is functional');
+
+let myItems = ['notebooks', 'pens', 'coffeecup', 'phone'];
+
+let myItemsSource$ = Rx.Observable.from(['notebooks', 'pens', 'coffeecup', 'phone']);
+
+const example = myItemsSource$.first();
+
+console.log('first', example);
+console.log('items', myItems);
+
+test('Test Two:', async t => {
   const bar = Promise.resolve('bar');
   t.is(await bar, 'bar');
 });
